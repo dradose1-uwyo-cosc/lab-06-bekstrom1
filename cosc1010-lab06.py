8fc719e16904225dfaedd98a67f4dcc2d7fac308
bekstrom1 <bekstro1@uwyo.edu>
2024-10-18 00:40:00 +0000

    Completed

--- cosc1010-lab06.py
+++ cosc1010-lab06.py
@@ -1,13 +1,9 @@
-# Your Name Here
+# Bradley Ekstrom
 # UWYO COSC 1010
-# Submission Date
+# Submission Date: 10/17/2024
 # Lab 06
-# Lab Section: 
-# Sources, people worked with, help given to: 
-# your
-# comments
-# here
-
+# Lab Section: 13
+# Sources, people worked with, help given to: Nolan Hottell, Riley Green
 
 random_string = """
 jppamiqxegokaizvkyawwurhewtcxohryzptznyuedhhmawpic
@@ -62,39 +58,50 @@ ibmkdwkqmdkjesqnjiqpijixbwjhenmsrrlpcseliiajlvcaac
 zkdenxczyooloczcaahnkehbwimvieedpdlqfafbqvxvfmvabd
 """
 random_string = random_string.replace("\n","") #remove all newline characters
-print(len(random_string)) # Print out the size for reference 
+print(len(random_string)) # Print out the size for reference
 
 # Above is a string with 2500 characters.
-# Create a program that goes through and counts the occurrence of each character, excluding \n using a  dictionary
+# Create a program that goes through and counts the occurrence of each character, excluding \n using a dictionary
 # Output each letter and its corresponding occurrence in alphabetical order
-# Output which letter occurred the most 
-# Output which letter occurred the least 
+# Output which letter occurred the most
+# Output which letter occurred the least
 # Output what the percentage of the string each character is, again in alphabetical
+letter_count = {}
+for letter in random_string:
+    if letter in letter_count:
+        letter_count[letter] += 1
+    else: 
+        letter_count[letter] = 1
+for letter in sorted(letter_count):
+    print(f'{letter}: {letter_count[letter]}')
+
 
 #Tips and trick:
 # You can iterate through strings like you would a list
-# All characters are lowercase 
-# Each letter will be PAIRED with its corresponding value 
+# All characters are lowercase
+# Each letter will be PAIRED with its corresponding value
 # That is to say, this is a great use of dictionaries
-    # You will  need to add the letter to the dictionary on first occurrence 
-    # Then increment its corresponding count 
-
-
+# You will need to add the letter to the dictionary on first occurrence
+# Then increment its corresponding count
 #Load all the elements into a dictionary
-#Will need to first declare a dictionary 
-
+#Will need to first declare a dictionary
 # Output: each letter and its corresponding occurrence in alphabetical order
 
 print("*"*75)
-# Output which letter occurred the most 
+# Output which letter occurred the most
 
-most_occurred = ""
-least_occurred = ""
+most_occurred = max(letter_count, key=letter_count.get)
+least_occured = min(letter_count, key=letter_count.get)
+
 
 print(f"The letter that occurred the most is {most_occurred}")
 print("*"*75)
-# Output which letter occurred the least 
-print(f"The letter that occurred the most is {least_occurred}")
+# Output which letter occurred the least
+print(f"The letter that occurred the most is {least_occured}")
 print("*"*75)
 
 # Output what the percentage of the string each character is, again in alphabetical
+total_characters = len(random_string)
+for letter in sorted(letter_count):
+    percentage = (letter_count[letter] / total_characters) * 100
+    print(f'{letter}: {percentage}%')
